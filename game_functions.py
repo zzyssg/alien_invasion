e6895045c54131a48aad7e788301c539eeedf390
zzyssg <zy1704528@buaa.edu.cn>
2021-01-19 17:17:09 +0800

    turn left and right

---
+++ game_functions.py
@@ -0,0 +1,29 @@
+import sys
+
+import pygame
+
+def check_events(ship):
+    """响应按键和鼠标事件"""
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                # 启动右移标志
+                ship.moving_right = True
+            elif event.key == pygame.K_LEFT:
+                ship.moving_left = True
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                # 关闭右移标志
+                ship.moving_right = False
+            elif event.key == pygame.K_LEFT:
+                ship.moving_left = False
+
+def update_screen(ai_settings,screen,ship):
+    # 重新绘制屏幕
+    screen.fill(ai_settings.bg_color)
+    ship.blitme()
+
+    # 让最近绘制的屏幕可见
+    pygame.display.flip()
